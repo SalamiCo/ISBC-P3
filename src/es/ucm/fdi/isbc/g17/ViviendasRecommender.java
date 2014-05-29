a5667c54e3806abf6351ecc676a33b9b963890b7
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2014-05-29 13:35:21 +0200

    ViviendasRecommender cycle first version implemented.

--- src/es/ucm/fdi/isbc/g17/ViviendasRecommender.java
+++ src/es/ucm/fdi/isbc/g17/ViviendasRecommender.java
@@ -1,13 +1,25 @@
 package es.ucm.fdi.isbc.g17;
 
+import java.util.Collection;
+
 import es.ucm.fdi.isbc.viviendas.ViviendasConnector;
 import es.ucm.fdi.isbc.viviendas.representacion.DescripcionVivienda;
 import jcolibri.casebase.LinealCaseBase;
 import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.Attribute;
+import jcolibri.cbrcore.CBRCase;
 import jcolibri.cbrcore.CBRCaseBase;
 import jcolibri.cbrcore.CBRQuery;
 import jcolibri.cbrcore.Connector;
 import jcolibri.exception.ExecutionException;
+import jcolibri.extensions.recommendation.casesDisplay.DisplayCasesTableMethod;
+import jcolibri.method.retrieve.RetrievalResult;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+import jcolibri.method.retrieve.NNretrieval.NNScoringMethod;
+import jcolibri.method.retrieve.NNretrieval.similarity.global.Average;
+import jcolibri.method.retrieve.NNretrieval.similarity.local.Equal;
+import jcolibri.method.retrieve.NNretrieval.similarity.local.Interval;
+import jcolibri.method.retrieve.selection.SelectCases;
 
 public final class ViviendasRecommender implements StandardCBRApplication {
 	
@@ -29,7 +41,47 @@ public final class ViviendasRecommender implements StandardCBRApplication {
 
 	@Override
 	public void cycle(CBRQuery query) throws ExecutionException {
+		// KNN
+		NNConfig simConfig = new NNConfig();
 		
+		// Global similarity function
+		simConfig.setDescriptionSimFunction(new Average());
+		
+		// Local similarity functions
+		simConfig.addMapping(new Attribute("tipo",DescripcionVivienda.class), new Equal());
+		simConfig.addMapping(new Attribute("localizacion",DescripcionVivienda.class), new Equal());
+		simConfig.addMapping(new Attribute("habitaciones",DescripcionVivienda.class), new Interval(12));
+		simConfig.addMapping(new Attribute("precio",DescripcionVivienda.class), new Interval(20000));
+		
+		// Execute NN
+		Collection<RetrievalResult> eval = NNScoringMethod.evaluateSimilarity(caseBase.getCases(), query, simConfig);
+		
+		// Select k cases
+		Collection<CBRCase> selectedcases = SelectCases.selectTopK(eval, 5);
+		
+		// Show result
+		DisplayCasesTableMethod.displayCasesInTableBasic(selectedcases);
+		/*Compute a direct proportion between the "Duration" and "Price" attributes.
+		NumericDirectProportionMethod.directProportion(	new Attribute("Duration",DescripcionVivienda.class), 
+				 											new Attribute("price",DescripcionVivienda.class), 
+				 											query, selectedcases);
+
+		// Compute a direct proportion between the "Duration" and "Price" attributes.
+		NumericDirectProportionMethod.directProportion(	new Attribute("NumberOfPersons",DescripcionVivienda.class), 
+				 											new Attribute("price",DescripcionVivienda.class), 
+				 											query, selectedcases);
+				 											*/
+		
+		// Revise
+
+		
+		// Retain
+		/*
+		retainDialog.showCases(selectedcases, caseBase.getCases().size());
+		retainDialog.setVisible(true);
+		Collection<CBRCase> casesToRetain = retainDialog.getCasestoRetain();
+		caseBase.learnCases(casesToRetain);
+		*/
 	}
 
 	@Override
