5c2be239179d1b6024d99c4b14e9eb4c4f8d31b5
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2014-05-29 12:05:05 +0200

    ViviendasRecommender first version.

--- src/es/ucm/fdi/isbc/g17/ViviendasRecommender.java
+++ src/es/ucm/fdi/isbc/g17/ViviendasRecommender.java
@@ -1,5 +1,6 @@
 package es.ucm.fdi.isbc.g17;
 
+import es.ucm.fdi.isbc.viviendas.representacion.DescripcionVivienda;
 import jcolibri.cbraplications.StandardCBRApplication;
 import jcolibri.cbrcore.CBRCaseBase;
 import jcolibri.cbrcore.CBRQuery;
@@ -7,33 +8,55 @@ import jcolibri.exception.ExecutionException;
 
 public final class ViviendasRecommender implements StandardCBRApplication {
 
-    @Override
-    public void configure () throws ExecutionException {
-        // TODO Auto-generated method stub
+	@Override
+	public void configure() throws ExecutionException {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public CBRCaseBase preCycle () throws ExecutionException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public CBRCaseBase preCycle() throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public void cycle (CBRQuery query) throws ExecutionException {
-        // TODO Auto-generated method stub
+	@Override
+	public void cycle(CBRQuery query) throws ExecutionException {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void postCycle () throws ExecutionException {
-        // TODO Auto-generated method stub
+	@Override
+	public void postCycle() throws ExecutionException {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    public static void main (String[] args) {
-        // TODO Auto-generated method stub
+	public static void main(String[] args) {
 
-    }
+		ViviendasRecommender recommender = new ViviendasRecommender();
+		//recommender.showMainFrame();
+		try {
+			recommender.configure();
+			recommender.preCycle();
+			
+			boolean cont = true;
+			while (cont) {
+				DescripcionVivienda desc = new DescripcionVivienda(0);
+				
+				CBRQuery query =  new CBRQuery();
+				query.setDescription(desc);
+				
+				recommender.cycle(query);
+				cont = false;
+			}
+			recommender.postCycle();
+		} catch (Exception e) {
+			org.apache.commons.logging.LogFactory.getLog(
+					ViviendasRecommender.class).error(e);
+			javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		System.exit(0);
+	}
 
 }
