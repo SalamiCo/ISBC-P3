70bbe66669e27d96055d41886242570271ca02e4
Daniel Escoz <darkhogg@gmail.com>
2014-05-29 11:29:36 +0200

    Recommender

---
+++ src/es/ucm/fdi/isbc/g17/ViviendasRecommender.java
@@ -0,0 +1,39 @@
+package es.ucm.fdi.isbc.g17;
+
+import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.CBRCaseBase;
+import jcolibri.cbrcore.CBRQuery;
+import jcolibri.exception.ExecutionException;
+
+public final class ViviendasRecommender implements StandardCBRApplication {
+
+    @Override
+    public void configure () throws ExecutionException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public CBRCaseBase preCycle () throws ExecutionException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void cycle (CBRQuery query) throws ExecutionException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void postCycle () throws ExecutionException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static void main (String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
