114635aab267e9baacff2f54cb14728b2c0259ef
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2014-06-08 18:46:51 +0200

    Card layout added.

--- src/es/ucm/fdi/isbc/g17/gui/MainFrame.java
+++ src/es/ucm/fdi/isbc/g17/gui/MainFrame.java
@@ -1,5 +1,6 @@
 package es.ucm.fdi.isbc.g17.gui;
 
+import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,7 +11,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
-import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
@@ -30,7 +30,10 @@ public final class MainFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private final String[] cards = {"Card1", "Card2"};
 	private ViviendasRecommender recommender;
+	
+	private JPanel bgpanel = new JPanel(new CardLayout());
 	private JComboBox comboVivienda = new JComboBox(TipoVivienda.values());
 	private JTextField textLocalidad = new JTextField();
 	private JSpinner spinHabitaciones = new JSpinner();
@@ -64,44 +67,64 @@ public final class MainFrame extends JFrame {
 
     /* package */void setupGui () {
     	setupContent();
+    	setupListeners();
         pack();
     }
     
     private void setupContent () {
-    	FormLayout layout = new FormLayout(
+    	setContentPane(bgpanel); 
+    	
+    	//panel1
+    	FormLayout layout1 = new FormLayout(
       	      "right:pref, 6dlu, 50dlu, 4dlu, default",  // columns
       	      "pref, 3dlu, pref, 3dlu, pref, 3dlu, pref");           // rows
-    	final JPanel panel = new JPanel(layout);
-        setContentPane(panel);        
+    	final JPanel panel1= new JPanel(layout1);               
         CellConstraints cc = new CellConstraints();
-        panel.add(new JLabel("Vivienda"),  cc.xy  (1, 1));
-        panel.add(comboVivienda,           cc.xywh(3, 1, 3, 1));
-        panel.add(new JLabel("Localidad"), cc.xy  (1, 3));
-        panel.add(textLocalidad,           cc.xy  (3, 3));
-        panel.add(new JLabel("Habitaciones"), cc.xy  (1, 5));
+        panel1.add(new JLabel("Vivienda"),  cc.xy  (1, 1));
+        panel1.add(comboVivienda,           cc.xywh(3, 1, 3, 1));
+        panel1.add(new JLabel("Localidad"), cc.xy  (1, 3));
+        panel1.add(textLocalidad,           cc.xy  (3, 3));
+        panel1.add(new JLabel("Habitaciones"), cc.xy  (1, 5));
         spinHabitaciones.setModel(new SpinnerNumberModel(1, 1, 10, 1));
-        panel.add(spinHabitaciones,        cc.xy  (3, 5));
-        panel.add(new JLabel("Precio"),    cc.xy  (1, 7));
+        panel1.add(spinHabitaciones,        cc.xy  (3, 5));
+        panel1.add(new JLabel("Precio"),    cc.xy  (1, 7));
         spinPrecio.setModel(new SpinnerNumberModel(350000, 50000, 5000000, 100000));
-        panel.add(spinPrecio,              cc.xy  (3, 7));
-        panel.add(buscarBtn,               cc.xy  (5, 7));
-        buscarBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed (final ActionEvent arg0) {
-            	try {
-	            	DescripcionVivienda initialDescription = new DescripcionVivienda(0);
-	            	initialDescription.setTipo(TipoVivienda.valueOf(comboVivienda.getSelectedItem().toString()));
-	            	initialDescription.setLocalizacion(textLocalidad.getText());
-	            	initialDescription.setHabitaciones(((Number)spinHabitaciones.getValue()).intValue());
-	            	initialDescription.setPrecio(((Number)spinPrecio.getValue()).intValue());
-	            	CBRQuery query =  new CBRQuery();
-					query.setDescription(initialDescription);
-					recommender.cycle(query);
-				} catch (ExecutionException e) {
-					e.printStackTrace();
-				}
-            }
-        });
+        panel1.add(spinPrecio,              cc.xy  (3, 7));
+        panel1.add(buscarBtn,               cc.xy  (5, 7));  
+        
+        //panel2
+        FormLayout layout2 = new FormLayout(
+        	      "right:pref, 6dlu, 50dlu, 4dlu, default",  // columns
+        	      "pref, 3dlu, pref, 3dlu, pref, 3dlu, pref");           // rows
+      	final JPanel panel2= new JPanel(layout2);  
+      	
+        bgpanel.add(panel1, cards[0]);
+        bgpanel.add(panel2, cards[1]);
+        
+    }
+    
+    private void setupListeners(){
+    	 buscarBtn.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed (final ActionEvent arg0) {
+             	try {
+ 	            	DescripcionVivienda initialDescription = new DescripcionVivienda(0);
+ 	            	initialDescription.setTipo(TipoVivienda.valueOf(comboVivienda.getSelectedItem().toString()));
+ 	            	initialDescription.setLocalizacion(textLocalidad.getText());
+ 	            	initialDescription.setHabitaciones(((Number)spinHabitaciones.getValue()).intValue());
+ 	            	initialDescription.setPrecio(((Number)spinPrecio.getValue()).intValue());
+ 	            	CBRQuery query =  new CBRQuery();
+ 					query.setDescription(initialDescription);
+ 					recommender.cycle(query);
+ 					
+ 					//cambiar al siguiente panel
+ 					CardLayout cl = (CardLayout)(bgpanel.getLayout());
+ 				    cl.show(bgpanel, cards[1]);
+ 				} catch (ExecutionException e) {
+ 					e.printStackTrace();
+ 				}
+             }
+         });
     }
 
 }
