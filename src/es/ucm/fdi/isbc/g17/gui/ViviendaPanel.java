30f0c89e1147048142f29660161c3299c2d3e343
Daniel Escoz <darkhogg@gmail.com>
2014-06-08 19:35:03 +0200

    More things

--- src/es/ucm/fdi/isbc/g17/gui/ViviendaPanel.java
+++ src/es/ucm/fdi/isbc/g17/gui/ViviendaPanel.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
@@ -15,7 +14,7 @@ public final class ViviendaPanel extends JPanel {
 
     private DescripcionVivienda description;
 
-    private JLabel title, price, rooms;
+    private JLabel title, price, rooms, location, bathrooms, surface, state;
 
     /**
      * Constructs a panel that shown no case.
@@ -57,22 +56,38 @@ public final class ViviendaPanel extends JPanel {
 
         FormLayout layout = new FormLayout( //
             "right:pref, 6dlu, left:pref", // columns
-            "pref, 3dlu, pref, 3dlu, pref"); // rows
+            "pref, 3dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu, pref"); // rows
         CellConstraints cc = new CellConstraints();
         setLayout(layout);
 
         add(title, cc.xyw(1, 1, 3));
 
         add(new JLabel("Precio"), cc.xy(1, 3));
-        add(new JTextField(), cc.xy(3, 3));
+        add(price, cc.xy(3, 3));
 
-        add(new JLabel("Habitaciones"), cc.xy(1, 5));
-        add(new JTextField(), cc.xy(3, 5));
+        add(new JLabel("Localización"), cc.xy(1, 5));
+        add(location, cc.xy(3, 5));
+
+        add(new JLabel("Habitaciones"), cc.xy(1, 7));
+        add(rooms, cc.xy(3, 7));
+
+        add(new JLabel("Baños"), cc.xy(1, 9));
+        add(bathrooms, cc.xy(3, 9));
+
+        add(new JLabel("Superficie"), cc.xy(1, 11));
+        add(surface, cc.xy(3, 11));
+
+        add(new JLabel("Estado"), cc.xy(1, 13));
+        add(state, cc.xy(3, 13));
     }
 
     private void updateInterface () {
         title.setText(description == null ? "" : description.getTitulo());
         price.setText(description == null ? "" : String.valueOf(description.getPrecio()));
         rooms.setText(description == null ? "" : String.valueOf(description.getHabitaciones()));
+        location.setText(description == null ? "" : String.valueOf(description.getLocalizacion()));
+        bathrooms.setText(description == null ? "" : String.valueOf(description.getLocalizacion()));
+        surface.setText(description == null ? "" : String.valueOf(description.getLocalizacion()));
+        state.setText(description == null ? "" : String.valueOf(description.getLocalizacion()));
     }
 }
